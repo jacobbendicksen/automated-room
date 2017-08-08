51664df611b5f216167428ac3134c037ec0207bf
Jacob Bendicksen <jacob@jacobbendicksen.com>
2017-08-07 22:06:24 -0700

    tethered arduino, maybe

--- j5.js
+++ j5.js
@@ -2,14 +2,24 @@
 
 var five = require('johnny-five');
 var Omega2 = require('./omega2-io');
-
-var board = new five.Board({
-  io: new Omega2()
+var SerialPort = require('serialport').SerialPort;
+var serialport = new SerialPort('/dev/ttyACM0', {
+  baudrate: 57600
 });
 
-board.on('ready', function () {
-  var led = new five.Led(0);
-  led.blink();
+var board = new five.Board({
+  // io: new Omega2()
+  port: serialport
+});
 
-  this.repl.inject({ led: led });
+board.on('ready', function() {
+  var led0 = new five.Led(0);
+  led0.blink();
+
+  var led13 = new five.Led(13);
+  led13.blink();
+
+  this.repl.inject({
+    led: led
+  });
 });
