c562632e536db5d91f4870ec61f220316edbd4cb
Jacob Bendicksen <jacob@jacobbendicksen.com>
2017-08-07 18:39:04 -0700

    restarting overhaul of omega2.js

--- omega2-io/lib/omega2.js
+++ omega2-io/lib/omega2.js
@@ -1,45 +1,39 @@
-'use strict';
-
 var FS = require( 'fs' );
 var CP = require( 'child_process' );
-var Emitter = require( 'events' ).EventEmitter;
+var Emitter = require( 'events' )
+	.EventEmitter;
 var Promise = require( 'bluebird' );
 var tick = process.setImmediate || process.nextTick;
-var _ = require( 'underscore' );
 
 var MODES = Object.freeze( require( './modes.json' ) );
 var pinGroups = require( './pingroups-omega2.json' );
 
 // translate lexical modes in json to numerical modes
-// pinGroups = pinGroups.map((group) => {
-//	group.modes = group.modes.map((mode) => {
-//		return modes[mode];
-//	})
-// })
-pinGroups = _.mapObject( pinGroups, ( group ) => {
-	group.modes = _.mapObject( group.modes, ( mode ) => {
-		return MODES[mode];
-	} );
-} );
-
-console.log( pinGroups );
+pinGroups = pinGroups.map( ( group ) => {
+	group.modes = group.modes.map( ( mode ) => {
+		return modes[ mode ];
+	} )
+} )
 
 // assign pin modes for each pin based on group membership
 var pinModes = [];
 for ( var groupName in pinGroups ) {
-	let group = pinGroups[groupName];
+	let group = pinGroups[ groupName ];
 	for ( var i = 0; i < group.pins.length; i++ ) {
 
-		var pin = pinModes[group.pins[i]] || { modes: [] };
+		var pin = pinModes[ group.pins[ i ] ] || {
+			modes: []
+		};
 		pin.modes = pin.modes.concat( group.modes );
-		pinModes[group.pins[i]] = pin;
+		pinModes[ group.pins[ i ] ] = pin;
 	}
 }
 
 // redundancy for group names
 pinGroups.ANALOG = pinGroups.PWM;
 
-var boards = [ ];
+
+var boards = [];
 var _i2cBus;
 var _i2cPollDelay; // delay before each i2c read in milliseconds
 
@@ -66,18 +60,18 @@ function Omega2( opts ) {
 		return p;
 	}, this );
 
-	boards[0] = this;
+	boards[ 0 ] = this;
 
 	this.defaultLed = 13;
 	this.isReady = false;
-	tick( function( ) {
+	tick( function() {
 		this.isReady = true;
 		this.emit( 'connect' );
 		this.emit( 'ready' );
 	}.bind( this ) );
 }
 
-Omega2.reset = function( ) {
+Omega2.reset = function() {
 	return null;
 };
 
@@ -98,26 +92,26 @@ Omega2.prototype = Object.create( Emitter.prototype, {
 
 Omega2.prototype.pinMode = function( pin, mode ) {
 	var pinIndex = pin;
-	this.pins[pinIndex].mode = mode;
+	this.pins[ pinIndex ].mode = mode;
 
 	switch ( mode ) {
 		case modes.OUTPUT:
 			CP.spawn( 'fast-gpio', [ 'set-output', pinIndex ] );
-			this.pins[pinIndex].mode = modes.OUTPUT;
-			this.pins[pinIndex].isPwm = false;
+			this.pins[ pinIndex ].mode = modes.OUTPUT;
+			this.pins[ pinIndex ].isPwm = false;
 			break;
 
 		case modes.INPUT:
 			CP.spawn( 'fast-gpio', [ 'set-input', pinIndex ] );
-			this.pins[pinIndex].mode = modes.INPUT;
-			this.pins[pinIndex].isPwm = false;
+			this.pins[ pinIndex ].mode = modes.INPUT;
+			this.pins[ pinIndex ].isPwm = false;
 			break;
 
 		case modes.ANALOG:
 			// intentional fallthrough
 		case modes.PWM:
-			this.pins[pinIndex].mode = modes.PWM;
-			this.pins[pinIndex].isPwm = true;
+			this.pins[ pinIndex ].mode = modes.PWM;
+			this.pins[ pinIndex ].isPwm = true;
 			break;
 
 		case modes.SERVO:
@@ -130,11 +124,11 @@ Omega2.prototype.pinMode = function( pin, mode ) {
 
 
 Omega2.prototype.analogWrite = function( pin, value, dutycycle ) {
-	if ( this.pins[pin].mode !== MODES.PWM ) {
+	if ( this.pins[ pin ].mode !== MODES.PWM ) {
 		this.pinMode( pin, MODES.PWM );
 	}
 
-	this.pins[pin].value = value;
+	this.pins[ pin ].value = value;
 
 	// set pin mode to PMW
 	// set pwm value
@@ -145,8 +139,8 @@ Omega2.prototype.analogWrite = function( pin, value, dutycycle ) {
 Omega2.prototype.pwmWrite = Omega2.prototype.analogWrite;
 
 Omega2.prototype.digitalRead = function( pinIndex, handler ) {
-	var pin = this.pins[pinIndex];
-	if ( this.pins[pinIndex].mode !== this.MODES.INPUT ) {
+	var pin = this.pins[ pinIndex ];
+	if ( this.pins[ pinIndex ].mode !== this.MODES.INPUT ) {
 		this.pinMode( pinIndex, this.MODES.INPUT );
 	}
 
@@ -177,12 +171,12 @@ Omega2.prototype.digitalRead = function( pinIndex, handler ) {
 };
 
 Omega2.prototype.digitalWrite = function( pin, value ) {
-	if ( this.pins[pin].mode !== this.MODES.OUTPUT ) {
+	if ( this.pins[ pin ].mode !== this.MODES.OUTPUT ) {
 		this.pinMode( pin, this.MODES.OUTPUT );
 	}
 
-	this.pins[pin].value = value;
-	CP.spawn( 'fast-gpio', [ 'set', pin, value] );
+	this.pins[ pin ].value = value;
+	CP.spawn( 'fast-gpio', [ 'set', pin, value ] );
 
 	return this;
 };
@@ -217,35 +211,35 @@ Omega2.prototype.i2cWrite = function( address, cmdRegOrData, inBytes ) {
 	 */
 	var buffer;
 
-	this.i2cConfig( );
+	this.i2cConfig();
 
 	// If i2cWrite was used for an i2cWriteReg call...
-	if (arguments.length === 3 && !Array.isArray( cmdRegOrData ) && !Array.isArray( inBytes )) {
+	if ( arguments.length === 3 && !Array.isArray( cmdRegOrData ) && !Array.isArray( inBytes ) ) {
 		return this.i2cWriteReg( address, cmdRegOrData, inBytes );
 	}
 
 	// Fix arguments if called with Firmata.js API
 	if ( arguments.length === 2 ) {
-		if (Array.isArray( cmdRegOrData )) {
-			inBytes = cmdRegOrData.slice( );
-			cmdRegOrData = inBytes.shift( );
+		if ( Array.isArray( cmdRegOrData ) ) {
+			inBytes = cmdRegOrData.slice();
+			cmdRegOrData = inBytes.shift();
 		} else {
-			inBytes = [ ];
+			inBytes = [];
 		}
 	}
 
 	// Only write if bytes provided
 	while ( inBytes.length ) {
-		var cp = CP.spawn( 'i2cset', [ '-y', '0', toHexString(cmdRegOrData), toHexString(inBytes.shift()) ]);
+		var cp = CP.spawn( 'i2cset', [ '-y', '0', toHexString( cmdRegOrData ), toHexString( inBytes.shift() ) ] );
 	}
 
 	return this;
 };
 
 Omega2.prototype.i2cWriteReg = function( address, register, value ) {
-	this.i2cConfig( );
+	this.i2cConfig();
 
-	var cp = CP.spawn( 'i2cset', [ '-y', '0', toHexString(address), toHexString(register), toHexString(value) ]);
+	var cp = CP.spawn( 'i2cset', [ '-y', '0', toHexString( address ), toHexString( register ), toHexString( value ) ] );
 
 	return this;
 };
@@ -254,7 +248,7 @@ Omega2.prototype._i2cRead = function( continuous, address, register, bytesToRead
 	var data;
 	var event = 'I2C-reply' + address + '-';
 
-	this.i2cConfig( );
+	this.i2cConfig();
 
 	// Fix arguments if called with Firmata.js API
 	if ( arguments.length === 4 && typeof register === 'number' && typeof bytesToRead === 'function' ) {
@@ -267,22 +261,22 @@ Omega2.prototype._i2cRead = function( continuous, address, register, bytesToRead
 
 	data = new Buffer( bytesToRead );
 
-	callback = typeof callback === 'function'
-		? callback
-		: function( ) {};
+	callback = typeof callback === 'function' ?
+		callback :
+		function() {};
 
-	event += register !== null
-		? register
-		: 0;
+	event += register !== null ?
+		register :
+		0;
 
-	var timeout = setTimeout( function read( ) {
+	var timeout = setTimeout( function read() {
 		var afterRead = function( err, bytesRead, buffer ) {
 			if ( err ) {
 				return this.emit( 'error', err );
 			}
 
 			// Convert buffer to Array before emit
-			this.emit(event, [ ].slice.call( buffer ));
+			this.emit( event, [].slice.call( buffer ) );
 
 			if ( continuous && --bytesRead ) {
 				setTimeout( read.bind( this ), _i2cDelay );
@@ -291,13 +285,15 @@ Omega2.prototype._i2cRead = function( continuous, address, register, bytesToRead
 
 		this.once( event, callback );
 
-		var args =  ['-y', '0', toHexString(address), toHexString(register)];
-		var cp = CP.spawn('i2cget', args);
-		cp.on('data', (data) => {
-			afterRead(null, 1, data);
-		});
+		var args = [ '-y'
+			'0', toHexString( address ), toHexString( register )
+		];
+		var cp = CP.spawn( 'i2cget', args );
+		cp.on( 'data', ( data ) => {
+			afterRead( null, 1, data );
+		} );
 
-	}.bind( this ), _i2cDelay);
+	}.bind( this ), _i2cDelay );
 
 	return this;
 };
@@ -309,7 +305,7 @@ Omega2.prototype._i2cRead = function( continuous, address, register, bytesToRead
 Omega2.prototype.i2cRead = function( address, register, bytesToRead, handler ) {
 	return this
 		._i2cRead
-		.apply(this, [ true ].concat([ ].slice.call( arguments )));
+		.apply( this, [ true ].concat( [].slice.call( arguments ) ) );
 };
 
 // this method supports both
@@ -319,7 +315,7 @@ Omega2.prototype.i2cRead = function( address, register, bytesToRead, handler ) {
 Omega2.prototype.i2cReadOnce = function( address, register, bytesToRead, handler ) {
 	return this
 		._i2cRead
-		.apply(this, [ false ].concat([ ].slice.call( arguments )));
+		.apply( this, [ false ].concat( [].slice.call( arguments ) ) );
 };
 
 var serialStates = {
@@ -330,9 +326,9 @@ var serialStates = {
 }
 
 Omega2.prototype.queryPinState = function( pinIndex, handler ) {
-	var pin = this.pins[pinIndex];
+	var pin = this.pins[ pinIndex ];
 
-	var event = ['change:pin.state'];
+	var event = [ 'change:pin.state' ];
 
 	var cp = CP.spawn( 'fast-gpio', [ 'get-direction', pinIndex ] );
 	cp.stdout.on( 'data', function( err, data ) {
@@ -351,8 +347,8 @@ Omega2.prototype.queryPinState = function( pinIndex, handler ) {
 			}
 		}
 		if ( changed ) {
-			event.push(pin);
-			this.emit.apply(this, event);
+			event.push( pin );
+			this.emit.apply( this, event );
 		}
 	} );
 	cp.on( 'exit', function() {
@@ -362,20 +358,20 @@ Omega2.prototype.queryPinState = function( pinIndex, handler ) {
 	return this;
 };
 
-Omega2.prototype.serialOpen = function ( baudRate, channel ){
+Omega2.prototype.serialOpen = function( baudRate, channel ) {
 	baudRate = baudRate || 115200;
 	channel = channel || 0;
 	address = '/dev/ttyS' + channel;
 
 	// set the baud rate on the port
-	CP.spawnSync('stty', ['-F', address, baudRate ]);
+	CP.spawnSync( 'stty', [ '-F', address, baudRate ] );
 
 
 	// open streams to sysfs node
 	var readStream = FS.ReadStream( address );
 	var writeStream = FS.WriteStream( address );
 
-	this.serial[channel] = {
+	this.serial[ channel ] = {
 		address: address,
 		baudRate: baudRate,
 		parity: parity,
@@ -387,45 +383,45 @@ Omega2.prototype.serialOpen = function ( baudRate, channel ){
 	}
 }
 
-Omega2.prototype.serialClose = function ( channel ){
+Omega2.prototype.serialClose = function( channel ) {
 	channel = channel || 0;
-	var serial = this.serial[channel];
+	var serial = this.serial[ channel ];
 	serial.readStream && serial.readStream.end();
 	serial.writeStream && serial.writeStream.end();
 	return this;
 }
 
-Omega2.prototype.serialListen = function ( messageTerminator, channel){
+Omega2.prototype.serialListen = function( messageTerminator, channel ) {
 	messageTerminator = messageTerminator || "\n";
 	channel = channel || 0;
 
-	var serial = this.serial[channel];
+	var serial = this.serial[ channel ];
 	serial.messageTerminator = messageTerminator;
-	if (serial.encoding !== encoding){
-		serial.readStream.setEncoding(encoding);
+	if ( serial.encoding !== encoding ) {
+		serial.readStream.setEncoding( encoding );
 	}
 
-	readStream.on('data', function( chunk ){
-		serial.buffer = serial.buffer.concat(Array.from(chunk));
+	readStream.on( 'data', function( chunk ) {
+		serial.buffer = serial.buffer.concat( Array.from( chunk ) );
 		this.serialOnMessage( serial );
-	});
+	} );
 	return this;
 }
 
-Omega2.prototype.serialOnMessage = function( serialObject ){
-	while(serialObject.indexOf(serialObject.messageTerminator) > -1){
-		var termIndex = serialObject.indexOf(serialObject.messageTerminator);
-		var message = buffer.splice(0, termIndex + 1);
+Omega2.prototype.serialOnMessage( serialObject ) {
+	while ( serialObject.indexOf( serialObject.messageTerminator ) > -1 ) {
+		var termIndex = serialObject.indexOf( serialObject.messageTerminator );
+		var message = buffer.splice( 0, termIndex + 1 );
 		serialObject.message = message;
-		this.emit('serial:message', serialObject);
+		this.emit( 'serial:message', serialObject );
 	}
 	return this;
 }
 
-Omega2.prototype.serialWrite = function ( message, encoding, channel ){
+Omega2.prototype.serialWrite = function( message, encoding, channel ) {
 	channel = channel || 0;
-	var serial = this.serial[channel];
-	serial.writeStream.write(message, encoding);
+	var serial = this.serial[ channel ];
+	serial.writeStream.write( message, encoding );
 	return this;
 }
 
@@ -452,24 +448,24 @@ Omega2.prototype.sendI2CWriteRequest = Omega2.prototype.i2cWrite;
 	'servoWrite',
 	'stepperConfig',
 	'stepperStep'
-	].forEach( function( method ) {
-	Omega2.prototype[method] = function( ) {
+].forEach( function( method ) {
+	Omega2.prototype[ method ] = function() {
 		throw method + ' is not yet implemented.';
 	};
 
-});
+} );
 
-function defer(){
+function defer() {
 	var deferred = {};
-	new Promise(function( _resolve, _reject ) {
+	new Promise( _resolve, _reject ) {
 		deferred.resolve = _resolve;
 		deferred.reject = _reject;
-	});
+	}
 	return deferred;
 }
 
-function toHexString( num ){
-	return '0x' + num.toString(16);
+function toHexString( num ) {
+	return '0x' + num.toString( 16 );
 }
 
 module.exports = Omega2;
