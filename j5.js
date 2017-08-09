0a2ad609a8e9aedb62b26558205168d72f195311
Jacob Bendicksen <jacob@jacobbendicksen.com>
2017-08-08 21:52:01 -0700

    turning off omega

--- j5.js
+++ j5.js
@@ -7,14 +7,14 @@ var serialport = new SerialPort('/dev/ttyACM0', {
   baudrate: 57600
 });
 
-var omega = new five.Board({
-  io: new Omega2()
-});
-
-omega.on('ready', function() {
-  var led = new five.Led(0);
-  led.blink();
-});
+// var omega = new five.Board({
+//   io: new Omega2()
+// });
+//
+// omega.on('ready', function() {
+//   var led = new five.Led(0);
+//   led.blink();
+// });
 
 var arduino = new five.Board({
   port: serialport
