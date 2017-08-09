a7e2834ebdc58b47491f8838f641fb14d91d21f4
Jacob Bendicksen <jacob@jacobbendicksen.com>
2017-08-08 21:01:49 -0700

    testing repl

--- j5.js
+++ j5.js
@@ -2,24 +2,24 @@
 
 var five = require('johnny-five');
 var Omega2 = require('./omega2-io');
-var SerialPort = require('serialport').SerialPort;
-var serialport = new SerialPort('/dev/ttyACM0', {
-  baudrate: 57600
-});
+// var SerialPort = require('serialport').SerialPort;
+// var serialport = new SerialPort('/dev/ttyACM0', {
+//   baudrate: 57600
+// });
 
 var board = new five.Board({
-  // io: new Omega2()
-  port: serialport
+  io: new Omega2()
+  // port: serialport
 });
 
 board.on('ready', function() {
   var led0 = new five.Led(0);
   led0.blink();
 
-  var led13 = new five.Led(13);
-  led13.blink();
+  // var led13 = new five.Led(13);
+  // led13.blink();
 
   this.repl.inject({
-    led: led
+    led: led0
   });
 });
